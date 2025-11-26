94ad48bddb3200ccf32f34fef997d440957d0c20
Luan Almeida <luancstalmeida@hotmail.com>
2025-11-26 11:29:54 -0300

    Primeria etapa ok. Criação da classLib e test.

---
+++ MoneExtension.tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneExtension.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        decimal money = 12.34m;
+        int cents = money.ToCents();
+        Assert.AreEqual(1234, cents);
+    }
+}
---
+++ MoneExtension/MoneyEx.cs
@@ -0,0 +1,22 @@
+﻿namespace MoneExtension;
+
+public static class MoneyEx
+{
+    public static int ToCents(this decimal money)
+    {
+        if (money < 0)
+        {
+            return 0;
+        }// negative money is not allowed
+        var text = money.ToString("0.00")
+                        .Replace(".", "")
+                        .Replace(",", "");// convert to string without decimal point
+        if(string.IsNullOrEmpty(text))
+            return 0;
+        
+        int.TryParse(text, out var result);// parse to int or return 0 if failed 
+        return result;
+    }
+    
+
+}
